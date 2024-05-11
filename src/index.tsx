cbec7d9db76609e6670acc2301a55a6aad76b7b1
fujiishingo0127 <shingo.fujii@alphadrive.co.jp>
2024-05-11 16:07:05 +0900

    2nd commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 export interface Env {
 }
-
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client'
 import { PrismaD1 } from '@prisma/adapter-d1'
@@ -11,14 +10,6 @@ type Bindings = {
 }
 
 const app = new Hono<{ Bindings: Bindings }>()
-
-app.get('/xxx', async (c) => {
-  const adapter = new PrismaD1(c.env.DB)
-  const prisma = new PrismaClient({ adapter })
-
-  const prefectures = await prisma.prefecture.findMany()
-  return c.json(prefectures)
-})
 
 app.get('/', (c) => {
   return c.html(
@@ -35,8 +26,28 @@ app.get('/', (c) => {
   )
 })
 
+//DBからjson取得
+app.get('/xxx', async (c) => {
+  //prismaを使用しDBコネクション
+  const adapter = new PrismaD1(c.env.DB)
+  const prisma = new PrismaClient({ adapter })
+  //findManyで取得
+  const prefectures = await prisma.prefecture.findMany()
+  //json形式で返す
+  return c.json(prefectures)
+})
+
+
 app.get('/api', (c) => {
   return c.json({ 今日のラッキーナンバー: 666 })
+})
+
+
+app.get('/posts/:id', (c) => {
+  const page = c.req.query('page')
+  const id = c.req.param('id')
+  c.header('X-Message', 'Hi!')
+  return c.text(`You want see ${page} of ${id}`)
 })
 
 export default app
